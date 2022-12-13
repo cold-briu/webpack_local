471c75393593848357b0ef78917ed2df36341927
Andrés Fernández <andresavena19@gmail.com>
2022-12-13 16:53:36 -0500

    feat: added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+	entry: {
+		dev: path.resolve(__dirname, "./src/index.js")
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, './build')
+	},
+
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env']
+				}
+			}
+		}]
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "myWebpackServer"
+		})
+	]
+
+}
